353dac4501a7c7894965ebe9ea1f81d2cf436f57
lundu <andricktaylor1@gmail.com>
2025-11-02 17:59:28 +0100

    test: ajouter la suite de tests de la couche Repository (ProductRepository)

--- src/test/java/com/eni/technostore/repository/ProductRepositoryTest.java
+++ src/test/java/com/eni/technostore/repository/ProductRepositoryTest.java
@@ -1,15 +1,18 @@
 package com.eni.technostore.repository;
 
 import com.eni.technostore.entity.Product;
+import com.eni.technostore.entity.ProductType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.time.Year;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
 @ActiveProfiles("test")
 @DataJpaTest
 class ProductRepositoryTest {
@@ -19,26 +22,69 @@ class ProductRepositoryTest {
 
     @Test
     @Sql("/test-data/products.sql")
-    void shouldFindAllProducts(){
-
-    /*
-        Afin de mieux structuré nos TU, nous alons appliquer la règle des 3A (Arrange, Act, Assert).
-
-     */
-        // Arrange : C’est la phase de mise en place des conditions du test: on va y déclarer et initialiser les objets nécessaires.
-        // Dans notre cas, le Arrange a déjà été fait lors de la préparation de nos data SQL.
-
-        /* ----------------------- */
-
-        // Act : C’est la phase où on exécutes l’action à tester : on appellera la méthode ou le comportement qu'on veut vérifier.
+    void shouldFindAllProductsTest(){
         List<Product> products = productRepository.findAll();
-
-        /* ----------------------- */
-
-        // Assert : C’est la phase où on constatera le résultat : on compares ce qu'on obtiens à ce qu'on attend.
-
+        assertNotNull(products);
         assertEquals(14, products.size());
+        assertEquals("Google", products.get(2).getBrand());
+    }
 
+    @Test
+    @Sql("/test-data/products.sql")
+    void shouldFindAllProductsByTypeTest(){
+        List<Product> products = productRepository.findByProductType(ProductType.TABLET);
+        assertNotNull(products);
+        assertEquals(3, products.size());
+        assertEquals("Microsoft", products.getLast().getBrand());
+    }
+
+    @Test
+    @Sql("/test-data/products.sql")
+    void shouldFindProductByIdTest(){
+        Product product = productRepository.findById(3L).orElseThrow(() -> new RuntimeException("Product not found"));
+        assertEquals("Google", product.getBrand());
+        assertEquals("Pixel 8 Pro", product.getModel());
+        assertEquals(ProductType.SMARTPHONE, product.getProductType());
+    }
+
+    @Test
+    @Sql("/test-data/products.sql")
+    void shouldSaveProductTest(){
+        Product product1 = new Product();
+        product1.setProductType(ProductType.HEADPHONES);
+        product1.setBrand("Bose");
+        product1.setModel("C45");
+        product1.setPrice(199.00);
+        product1.setYear(Year.of(2016));
+
+        Product savedProduct = productRepository.save(product1);
+        assertNotNull(savedProduct);
+        assertNotNull(savedProduct.getId());
+        assertEquals(15L, savedProduct.getId().longValue());
+        assertNotEquals("JBL", savedProduct.getBrand());
+    }
+
+    @Test
+    @Sql("/test-data/products.sql")
+    void showUpdateProductTest(){
+        Product product = productRepository.findById(11L).orElseThrow(() -> new RuntimeException("Product not found"));
+        assertEquals("Apple", product.getBrand());
+
+        product.setBrand("HUAWEI");
+        Product updatedProduct = productRepository.save(product);
+        assertEquals("HUAWEI", updatedProduct.getBrand());
+
+        updatedProduct.setPrice(1000.00);
+        productRepository.save(updatedProduct);
+        assertEquals(1000.00, updatedProduct.getPrice());
+    }
+
+    @Test
+    @Sql("/test-data/products.sql")
+    void shouldDeleteProductTest(){
+        assertEquals(14, productRepository.count());
+        productRepository.deleteById(12L);
+        assertEquals(13, productRepository.count());
     }
 
 }
